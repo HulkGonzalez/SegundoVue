16b111cf2461d7c18ddc97f5f48531a7dced4662
Cesar Gonzalez <cesargonzalezdc@gmail.com>
2022-02-16 19:36:06 -0300

    Estructura Vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Crear una nueva tarea</h1>
+    <form v-on:submit.prevent="crearTarea">
+      <p>Tarea: <input type="text" v-model="nombre" /></p>
+      <button type="submit">Crear</button>
+    </form>
+
+    <h2>Lista</h2>
+
+    <ul class="" v-for="(tarea, index) in tareas" :key="tarea">
+      <li class="">{{ index }} - {{ tarea.nombre }}</li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "App",
+ 
+  data() {
+    return {
+      nombre: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    crearTarea: function () {
+      this.tareas.push({
+        nombre: this.nombre,
+      });
+    },
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
